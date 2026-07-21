d5b1a65d7b9da5fcf40d40c70fb619af0981f2cd
MCode <hashamdar.com@gmail.com>
2026-07-21 15:12:35 +0330

    Add files via upload

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,6 @@
+export const footerLinks = [
+    { title: 'Collections', items: ['RM 011', 'RM 078', 'RM 11-03', 'Limited Edition'] },
+    { title: 'Heritage', items: ['Our Story', 'Craftsmanship', 'Innovation'] },
+    { title: 'Support', items: ['Contact Us', 'Service', 'Warranty'] },
+    { title: 'Legal', items: ['Privacy Policy', 'Terms of Service'] },
+];
---
+++ src/components/heritage/heritage.jsx
@@ -0,0 +1,23 @@
+export const heriTagData = [
+    {
+        id : 1, 
+        img: "./src/pic/watch-movement.jpg.webp",
+        subtitle: "Established 1894",
+        mainTitle: "A legacy <br/> precision & Art.",
+        description: "Every timepiece is a masterpiece, born from centuries of tradition."
+    }, 
+    {
+        id : 2, 
+        img: "./src/pic/watchmaking.jpg",
+        subtitle: "The Craftsmanship",
+        mainTitle: "Mastery in <br/> Every Detail",
+        description: "Hand-assembled by the world's finest watchmakers."
+    },
+    {
+        id : 3, 
+        img: "./src/pic/image1s.jpg",
+        subtitle: "The Future",
+        mainTitle: "Defining the <br/> Next Era",
+        description: "Where tradition meets the boundaries of modern engineering."
+    }
+];
